5bafcda87e3a33c0945dd217920f811d333a1328
Pasindu Akalpa <43250159+pAkalpa@users.noreply.github.com>
2020-10-12 17:19:22 +0530

    Add files via upload

---
+++ Simple Constructer/examMain.cs
@@ -0,0 +1,14 @@
+using Curtin;
+using System;
+
+namespace mainProg
+{
+    class mainP
+    {
+        static void Main(string[] args)
+        {
+            exam e = new exam(10,20,30);
+            e.displayMarks();
+        }
+    }
+}
